9aa6bc2513acd41d963b33ab0117be9a230d0712
ycyaoxdu <yaoyuchen0626@163.com>
2021-12-26 19:01:52 +0800

    modified aes algorithm: mix colomn
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- pkg/aesEncodeWithAvxSpeedUp/avx_aes_c/avx_aes.c
+++ pkg/aesEncodeWithAvxSpeedUp/avx_aes_c/avx_aes.c
@@ -354,6 +354,42 @@ __m256i avx_set_data_u16(uint16_t *in)
     return _mm256_load_si256((const __m256i *)in);
 }
 
+//
+__m256i avx_left_shift_step(__m256i state)
+{
+    __m256i res = _mm256_or_si256(
+        _mm256_slli_epi64(state, 0x30),
+        _mm256_srli_epi64(state, 0x10));
+
+    // printf("avx_left_shift_step:\n");
+    // avx_print_u16(&res);
+    return res;
+}
+//
+__m256i avx_right_shift_step(__m256i state)
+{
+
+    __m256i res = _mm256_or_si256(
+        _mm256_slli_epi64(state, 0x10),
+        _mm256_srli_epi64(state, 0x30));
+
+    // printf("avx_right_shift_step raw:\n");
+    // avx_print_u16(&state);
+    // printf("avx_right_shift_step:\n");
+    // avx_print_u16(&res);
+    return res;
+}
+//
+__m256i avx_up_shift_step(__m256i state)
+{
+    __m256i res = _mm256_permute4x64_epi64(state, 0b00111001);
+
+    // printf("avx_up_shift_step raw:\n");
+    // avx_print_u16(&state);
+    // printf("avx_up_shift_step:\n");
+    // avx_print_u16(&res);
+    return res;
+}
 // return round key for specified round i.
 uint8_t *get_round_key(int i, uint8_t *expanded_key)
 {
@@ -389,7 +425,7 @@ __m256i avx_add_round_key(__m256i state, __m256i round_key)
     return res;
 }
 
-// 
+//
 __m256i avx_sub_bytes(__m256i in_state)
 {
     printf("in avx_sub_bytes...\n");
@@ -465,26 +501,17 @@ __m256i avx_mix_colomn_helper(__m256i state, __m256i TT)
     __m256i avx_mask_int16 = avx_set_data_u16(p_mask_int16);
     __m256i avx_multiplier = avx_set_data_u16(p_multiplier);
     //
-    // printf("avx_T:\n");
-    // avx_print_u16(&avx_T);
-    // printf("avx_mask_bit:\n");
-    // avx_print_u16(&avx_mask_bit);
-    // printf("avx_multiplier:\n");
-    // avx_print_u16(&avx_multiplier);
-
-    //
     __m256i s_left_1 = _mm256_and_si256(avx_mask_int16, _mm256_slli_epi16(state, 1));
     //
-    __m256i t_and_mask = _mm256_and_si256(TT, avx_mask_bit);
+    __m256i state_and_t_and_mask = _mm256_and_si256(state, _mm256_and_si256(TT, avx_mask_bit));
     //
     __m256i s_right_7 = _mm256_srli_epi16(state, 7);
     __m256i t_right_1 = _mm256_srli_epi16(TT, 1);
-    __m256i tr1_and_sr7 = _mm256_and_si256(t_right_1, s_right_7);
+    __m256i tr1_and_sr7_and_mul = _mm256_and_si256(avx_multiplier, _mm256_and_si256(t_right_1, s_right_7));
 
-    __m256i res = _mm256_xor_si256(state,
+    __m256i res = _mm256_xor_si256(state_and_t_and_mask,
                                    _mm256_xor_si256(s_left_1,
-                                                    _mm256_xor_si256(t_and_mask,
-                                                                     _mm256_xor_si256(tr1_and_sr7, avx_multiplier))));
+                                                    tr1_and_sr7_and_mul));
 
     // printf("s:\n");
     // avx_print_u16(&state);
@@ -505,38 +532,8 @@ __m256i avx_mix_colomn_helper(__m256i state, __m256i TT)
     // printf("tr1_and_sr7:\n");
     // avx_print_u16(&tr1_and_sr7);
 
-    // __m256i res = _mm256_xor_si256(state,
-    //                                _mm256_xor_si256(_mm256_and_si256(avx_T, avx_mask_bit),
-    //                                                 _mm256_xor_si256(_mm256_slli_epi16(state, 1),
-    //                                                                  _mm256_xor_si256(avx_multiplier,
-    //                                                                                   _mm256_and_si256(_mm256_srli_epi16(state, 7),
-    //                                                                                                        _mm256_srli_epi16(avx_T, 1))))));
-
     printf("avx_mix_colomn_helper:\n");
     avx_print_u16(&res);
-    return res;
-}
-
-//
-__m256i avx_left_shift_step(__m256i state)
-{
-    __m256i res = _mm256_or_si256(
-        _mm256_slli_epi64(state, 0x30),
-        _mm256_srli_epi64(state, 0x10));
-
-    // printf("avx_left_shift_step:\n");
-    // avx_print_u16(&res);
-    return res;
-}
-//
-__m256i avx_up_shift_step(__m256i state)
-{
-    __m256i res = _mm256_permute4x64_epi64(state, 0b00111001);
-
-    // printf("avx_up_shift_step raw:\n");
-    // avx_print_u16(&state);
-    // printf("avx_up_shift_step:\n");
-    // avx_print_u16(&res);
     return res;
 }
 
@@ -560,9 +557,8 @@ __m256i avx_mix_colomn_add_helper(__m256i state)
     __m256i mul3 = avx_mix_colomn_helper(su3, tl3);
 
     //
-    __m256i res1 = _mm256_xor_si256(mul2,mul3);
-    __m256i res2 = _mm256_xor_si256(mul0,mul1);
-
+    __m256i res1 = _mm256_xor_si256(mul2, mul3);
+    __m256i res2 = _mm256_xor_si256(mul0, mul1);
     __m256i res = _mm256_xor_si256(res1, res2);
 
     printf("avx_mix_colomn_add_helper:\n");
@@ -582,20 +578,21 @@ __m256i avx_mix_column(__m256i state)
     __m256i avx_selector2 = avx_set_data_u16(p_selector2);
     __m256i avx_selector3 = avx_set_data_u16(p_selector3);
     __m256i avx_selector4 = avx_set_data_u16(p_selector4);
-
-    __m256i state_l1 = avx_left_shift_step(state);
+    //
+    __m256i state_l0 = state;
+    __m256i state_l1 = avx_left_shift_step(state_l0);
     __m256i state_l2 = avx_left_shift_step(state_l1);
     __m256i state_l3 = avx_left_shift_step(state_l2);
-
     //
-    __m256i res_part1 = _mm256_and_si256(avx_mix_colomn_add_helper(state), avx_selector1);
-    __m256i res_part2 = _mm256_and_si256(avx_mix_colomn_add_helper(state), avx_selector2);
-    __m256i res_part3 = _mm256_and_si256(avx_mix_colomn_add_helper(state), avx_selector3);
-    __m256i res_part4 = _mm256_and_si256(avx_mix_colomn_add_helper(state), avx_selector4);
+    __m256i raw_res_part1 = _mm256_and_si256(avx_mix_colomn_add_helper(state_l0), avx_selector1);
+    __m256i raw_res_part2 = _mm256_and_si256(avx_mix_colomn_add_helper(state_l1), avx_selector1);
+    __m256i raw_res_part3 = _mm256_and_si256(avx_mix_colomn_add_helper(state_l2), avx_selector1);
+    __m256i raw_res_part4 = _mm256_and_si256(avx_mix_colomn_add_helper(state_l3), avx_selector1);
 
-    __m256i res = _mm256_or_si256(res_part1,
-                                  _mm256_or_si256(res_part2,
-                                                  _mm256_or_si256(res_part3, res_part4)));
+    __m256i res = _mm256_or_si256(raw_res_part1,
+                                  _mm256_or_si256(avx_right_shift_step(raw_res_part2),
+                                                  _mm256_or_si256(avx_right_shift_step(avx_right_shift_step(raw_res_part3)),
+                                                                  avx_right_shift_step(avx_right_shift_step(avx_right_shift_step(raw_res_part4))))));
 
     printf("avx_mix_column:\n");
     avx_print_u16(&res);
