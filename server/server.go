5857324806e253247659a3c6679db8c115cf6373
ycyaoxdu <yaoyuchen0626@163.com>
2021-12-30 17:49:09 +0800

    add cors
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- server/server.go
+++ server/server.go
@@ -3,11 +3,20 @@ package server
 import (
 	"github.com/ycyaoxdu/AESEncodeServer/server/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func RunServer() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
+
 	router.SetRouter(r)
 	r.Run(":8086") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
